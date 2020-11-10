17b0c78d5a20b3886b45d4976aba7cb64da32dd0
nawawishkid <nawawish.samerpark@gmail.com>
2020-11-10 23:27:33 +0700

    fix: export sata step failed exception

--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
-export { SagaCompensationFailed, SagaExecutionFailed } from './src/exceptions';
+export {
+  SagaCompensationFailed,
+  SagaExecutionFailed,
+  SagaStepInvocationFailed,
+  SagaStepCompensationFailed,
+} from './src/exceptions';
 export { Saga, SagaStates } from './src/saga';
 export { SagaBuilder } from './src/saga-builder';
 export { SagaBuilderFactory } from './src/saga-builder-factory';
